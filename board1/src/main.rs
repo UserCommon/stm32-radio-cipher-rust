8de2ff451fd14e9a2a2e3263133becd7584f7b3c
usercommon <kirilnenashev@gmail.com>
2024-09-29 21:40:33 +0300

    feat: board1 succesfully transmits data

--- board1/src/main.rs
+++ board1/src/main.rs
@@ -4,7 +4,7 @@
 
 use defmt::{info, unwrap};
 use embassy_executor::Spawner;
-use embassy_stm32::{gpio::{Input, Level, Output, Pull, Speed}, peripherals};
+use embassy_stm32::{gpio::{Input, Level, Output, Pull, Speed}, peripherals::{self, DMA1_CH3, DMA1_CH4, PB3, PB4, PB5, SPI2}};
 use embassy_stm32::spi;
 use embassy_time::{Timer, Duration};
 use {defmt_rtt as _, panic_probe as _};
@@ -26,16 +26,23 @@ async fn main(spawner: Spawner) {
         p.DMA1_CH3,     // RX DMA channel (not used, will be removed in the future)
         spi_config
     ); // PB13 (SCLK), PB15 (SDIO)
-
      // Configure the Chip Select (CS) pin on PB4
     let mut cs = Output::new(p.PB4, Level::High, Speed::Medium);
-
     // Configure FCSB pin on PB5 as output
     let mut fcsb = Output::new(p.PB5, Level::High, Speed::Medium);
-
     // Configure NIQR pin on PB3 as input (interrupt)
     let irq = Input::new(p.PB3, Pull::Up);
+    unwrap!(spawner.spawn(tx(spi, cs, fcsb, irq)));
+    
+}
 
+#[embassy_executor::task]
+async fn tx(
+    mut spi: spi::Spi<'static, SPI2, DMA1_CH4, DMA1_CH3>,
+    mut cs: Output<'static, PB4>,
+    mut fcsb: Output<'static, PB5>,
+    mut irq: Input<'static, PB3>
+) {
     // Reset the module
     cs.set_low();
     embassy_time::Timer::after(Duration::from_millis(10)).await;
@@ -45,23 +52,25 @@ async fn main(spawner: Spawner) {
     let mut read_buf: [u16; 2] = [0x00; 2]; // Buffer for reading data
     let write_buf: [u16; 2] = [0xAA, 0xBB]; // Data to be written
 
-    // Perform SPI transfer
-    cs.set_low(); // Select the device
-    let result = spi.blocking_transfer(&mut read_buf, &write_buf);
-    cs.set_high(); // Deselect the device
+    loop {
+        // Perform SPI transfer
+        cs.set_low(); // Select the device
+        let result = spi.blocking_transfer(&mut read_buf, &write_buf);
+        cs.set_high(); // Deselect the device
 
-    match result {
-        Ok(_) => {
-            // Handle successful transfer
-            defmt::info!("SPI transfer successful: Read = {:?}", read_buf);
+        match result {
+            Ok(_) => {
+                // Handle successful transfer
+                defmt::info!("SPI transfer successful: Read = {:?}", read_buf);
+            }
+            Err(e) => {
+                // Handle SPI transfer error
+                defmt::error!("SPI transfer error: {:?}", e);
+            }
         }
-        Err(e) => {
-            // Handle SPI transfer error
-            defmt::error!("SPI transfer error: {:?}", e);
-        }
+        Timer::after(Duration::from_millis(2000)).await;
     }
 }
-
 
 #[embassy_executor::task]
 async fn blinker(mut led: Output<'static, peripherals::PC13>, interval: Duration) {
